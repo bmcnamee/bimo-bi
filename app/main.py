36bc321668063dca25cbe585ed420b886a91bb9b
bmcnamee <860221+bmcnamee@users.noreply.github.com>
2022-11-20 09:11:27 -0800

    get google sheet url from secrets

--- app/main.py
+++ app/main.py
@@ -8,14 +8,14 @@ credentials = service_account.Credentials.from_service_account_info(
     scopes=["https://www.googleapis.com/auth/spreadsheets", ],
 )
 gc = gspread.authorize(credentials)
-sh_url = "https://docs.google.com/spreadsheets/d/1NkNIp7hSoqBGZR82Goflk60MlCrYjx19MtwV4DW4-QM/edit?usp=sharing"
-# sh_url = st.secrets["private_gsheets_url"]
+# sh_url = "https://docs.google.com/spreadsheets/d/1NkNIp7hSoqBGZR82Goflk60MlCrYjx19MtwV4DW4-QM/edit?usp=sharing"
+sh_url = st.secrets["private_gsheets_url"]
 sh = gc.open_by_url(sh_url)
 ws_name = "Sheet1"
 ws = sh.worksheet(ws_name)
 a1 = ws.acell("A1").value
 df = pd.DataFrame(ws.get_all_records())
-st.markdown(f"# BIMO BI Demo.")
+st.markdown(f"# BIMO BI Demo")
 st.markdown(f"{type(df)}")
 st.markdown(f"{df.shape}")
 st.dataframe(df.head())

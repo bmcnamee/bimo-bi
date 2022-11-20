990f709a4f70ffcb2c1102c4cd3b787b0177914a
bmcnamee <860221+bmcnamee@users.noreply.github.com>
2022-11-20 08:51:31 -0800

    added google-auth to requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 # import pandas as pd
 import gspread
-# from google.oauth2 import service_account
+from google.oauth2 import service_account
 
 # credentials = service_account.Credentials.from_service_account_info(
 #     st.secrets["gcp_service_account"],

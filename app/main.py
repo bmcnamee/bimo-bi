7ed406581cb249894e1a5fc649327321fd324532
bmcnamee <860221+bmcnamee@users.noreply.github.com>
2022-11-20 09:00:29 -0800

    get data from google sheet

--- app/main.py
+++ app/main.py
@@ -7,12 +7,14 @@ credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
     scopes=["https://www.googleapis.com/auth/spreadsheets", ],
 )
-# gc = gspread.authorize(credentials)
+gc = gspread.authorize(credentials)
+sh_url = "https://docs.google.com/spreadsheets/d/1NkNIp7hSoqBGZR82Goflk60MlCrYjx19MtwV4DW4-QM/edit?usp=sharing"
 # sh_url = st.secrets["private_gsheets_url"]
-# sh = gc.open_by_url(sh_url)
-# ws_name = "Sheet1"
-# ws = sh.worksheet(ws_name)
-# a1 = ws.acell("A1").value
+sh = gc.open_by_url(sh_url)
+ws_name = "Sheet1"
+ws = sh.worksheet(ws_name)
+a1 = ws.acell("A1").value
 st.markdown(f"# BIMO BI Demo.")
-st.markdown(f"{type(credentials)}")
-st.markdown(f"{dir(credentials)}")
+st.markdown(f"{type(gc)}")
+st.markdown(f"{dir(gc)}")
+st.markdown(f"{a1}")

91d9bb2c4205bb89a18e6b8fca1a4884fd2eefa9
bmcnamee <860221+bmcnamee@users.noreply.github.com>
2022-11-20 09:25:20 -0800

    length of stay chart

--- app/main.py
+++ app/main.py
@@ -1,21 +1,27 @@
-import streamlit as st
-import pandas as pd
+import streamlit as st  # loaded by streamlit cloud
+import pandas as pd  # loaded by streamlit cloud
 import gspread
-from google.oauth2 import service_account
+from google.oauth2 import service_account  # module name is google-auth
 
+# credentials not saved to Github. Saved in settings in streamlit cloud
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
     scopes=["https://www.googleapis.com/auth/spreadsheets", ],
 )
 gc = gspread.authorize(credentials)
-# sh_url = "https://docs.google.com/spreadsheets/d/1NkNIp7hSoqBGZR82Goflk60MlCrYjx19MtwV4DW4-QM/edit?usp=sharing"
 sh_url = st.secrets["private_gsheets_url"]
 sh = gc.open_by_url(sh_url)
 ws_name = "Sheet1"
 ws = sh.worksheet(ws_name)
-a1 = ws.acell("A1").value
 df = pd.DataFrame(ws.get_all_records())
-st.markdown(f"# BIMO BI Demo")
+
+st.markdown(f"# Episode of Care Count by Length of Stay")
+_df = df.groupby("length_of_stay").agg({"episode_id": "count"})
+st.bar_chart(
+    _df,
+    x="length_of_stay",
+    y="episode_id",
+)
 st.markdown(f"{type(df)}")
 st.markdown(f"{df.shape}")
 st.dataframe(df.head())

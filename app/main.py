00d845fc4d481af3cb33d7f74856f694e72365ec
bmcnamee <860221+bmcnamee@users.noreply.github.com>
2022-11-20 11:18:30 -0800

    updated length of stay chart

--- app/main.py
+++ app/main.py
@@ -18,20 +18,23 @@ df = pd.DataFrame(ws.get_all_records())
 
 st.markdown(f"# Episode of Care Count")
 st.markdown(f"## By Length of Stay")
-_df = df.groupby("length_of_stay", as_index=False).agg({"episode_id": "count"})
-st.bar_chart(
-    _df,
-    x="length_of_stay",
-    y="episode_id",
+_df = df.groupby("length_of_stay", as_index=False).agg({"episode_id": "count", "satisfaction_rating": "mean"})
+_base = alt.Chart(_df).encode(
+    alt.X("length_of_stay",
+          axis=alt.Axis(title="Length of Stay"))
 )
+_bar = _base.mark_bar(color='#4c78a8').encode(
+    alt.Y("episode_id",
+          axis=alt.Axis(title="Episode Count", titleColor="#4c78a8"))
+)
+_line = _base.mark_line(color='#F18727').encode(
+    alt.Y("satisfaction_rating",
+          axis=alt.Axis(title="Average Satisfaction Rating", titleColor="#F18727"))
+)
+_chart = alt.layer(_bar, _line).resolve_scale(y="independent")
+st.altair_chart(_chart, use_container_width=True)
+
 st.markdown(f"## By Age")
-_df = df.groupby("age", as_index=False).agg({"episode_id": "count"})
-st.bar_chart(
-    _df,
-    x="age",
-    y="episode_id",
-)
-st.markdown(f"## By Age (Altair)")
 _df = df.groupby("age", as_index=False).agg({"episode_id": "count", "satisfaction_rating": "mean"})
 _base = alt.Chart(_df).encode(
     alt.X("age",

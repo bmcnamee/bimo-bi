0691eb6e51e7c7719cd03d317fd60abca8a010e1
bmcnamee <860221+bmcnamee@users.noreply.github.com>
2022-11-20 08:27:06 -0800

    testing min requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
 import streamlit as st
-import pandas as pd
+# import pandas as pd
 import gspread
-from google.oauth2 import service_account
+# from google.oauth2 import service_account
 
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=["https://www.googleapis.com/auth/spreadsheets",],
-)
-gc = gspread.authorize(credentials)
-sh_url = st.secrets["private_gsheets_url"]
-sh = gc.open_by_url(sh_url)
-ws_name = "Sheet1"
-ws = sh.worksheet(ws_name)
-a1 = ws.acell("A1").value
-st.markdown(f"# BIMO BI Demo. Cell A1 = {a1}")
+# credentials = service_account.Credentials.from_service_account_info(
+#     st.secrets["gcp_service_account"],
+#     scopes=["https://www.googleapis.com/auth/spreadsheets",],
+# )
+# gc = gspread.authorize(credentials)
+# sh_url = st.secrets["private_gsheets_url"]
+# sh = gc.open_by_url(sh_url)
+# ws_name = "Sheet1"
+# ws = sh.worksheet(ws_name)
+# a1 = ws.acell("A1").value
+st.markdown(f"# BIMO BI Demo.")
+st.markdown(f"{dir(gspread)}")
